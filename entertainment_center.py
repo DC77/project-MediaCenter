f5ee2b074a636e901d2226dba4b8cb857a640fda
DC77 <dave.ciampa@gmail.com>
2015-09-14 13:15:06 -0400

    Formatting updates

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,43 @@
-__author__ = 'daveCiampa'
-
 import media
 import fresh_tomatoes
 
 # following calls the class from media.py to use instance variables
-rush = media.Movie("Rush", "History of two of the best F1 drivers.",
-                        "https://upload.wikimedia.org/wikipedia/en/d/d0/Rush_UK_poster.jpeg",
-                        "https://www.youtube.com/watch?v=L_u3FODrenM")
+rush = media.Movie(
+    "Rush",
+    "History of two of the best F1 drivers.",
+    "https://upload.wikimedia.org/wikipedia/en/d/d0/Rush_UK_poster.jpeg",
+    "https://www.youtube.com/watch?v=L_u3FODrenM")
 
-pacific_rim = media.Movie("Pacific Rim", "Aliens (from the ocean!) vs. Mechs.",
-                        "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",
-                        "https://www.youtube.com/watch?v=5guMumPFBag")
+pacific_rim = media.Movie(
+    "Pacific Rim",
+    "Aliens (from the ocean!) vs. Mechs.",
+    "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",  # noqa
+    "https://www.youtube.com/watch?v=5guMumPFBag")
 
-love_actually = media.Movie("Love Actually", "If you look for it, I've got a sneaky feeling you'll find that love actually is all around.",
-                        "https://upload.wikimedia.org/wikipedia/en/e/eb/Love_Actually_movie.jpg",
-                        "https://www.youtube.com/watch?v=_eCb8w8SRAk")
+love_actually = media.Movie(
+    "Love Actually",
+    "If you look for it, I've got a sneaky feeling you'll find that love \
+        actually is all around.",
+    "https://upload.wikimedia.org/wikipedia/en/e/eb/Love_Actually_movie.jpg",
+    "https://www.youtube.com/watch?v=_eCb8w8SRAk")
 
-interstellar = media.Movie("Interstellar", "The world is dieing, humanity has to find a new home.",
-                        "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
-                        "https://www.youtube.com/watch?v=0vxOhd4qlnA")
+interstellar = media.Movie(
+    "Interstellar",
+    "The world is dieing, humanity has to find a new home.",
+    "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=0vxOhd4qlnA")
 
-elf = media.Movie("Elf", "A confused Elf explores New York.",
-                        "https://upload.wikimedia.org/wikipedia/en/d/df/Elf_movie.jpg",
-                        "https://www.youtube.com/watch?v=_TOQWF_-RWY")
+elf = media.Movie(
+    "Elf",
+    "A confused Elf explores New York.",
+    "https://upload.wikimedia.org/wikipedia/en/d/df/Elf_movie.jpg",
+    "https://www.youtube.com/watch?v=_TOQWF_-RWY")
 
-guardians = media.Movie("Guardians of the Galaxy", "Group of misfit aliens saves the galaxy.",
-                        "https://upload.wikimedia.org/wikipedia/en/8/8f/GOTG-poster.jpg",
-                        "https://www.youtube.com/watch?v=B16Bo47KS2g")
+guardians = media.Movie(
+    "Guardians of the Galaxy",
+    "Group of misfit aliens saves the galaxy.",
+    "https://upload.wikimedia.org/wikipedia/en/8/8f/GOTG-poster.jpg",
+    "https://www.youtube.com/watch?v=B16Bo47KS2g")
 
 movies = [rush, pacific_rim, love_actually, interstellar, elf, guardians]
 fresh_tomatoes.open_movies_page(movies)
-
-
